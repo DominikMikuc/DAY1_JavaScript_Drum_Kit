0d116db6f9a159461880343bcdb5160dc5e91400
ka3vas <tyrnis@gmail.com>
2021-08-25 14:20:52 +0200

    fix: missing closing bracket

--- DAY1_JavaScript_Drum_Kit/app.js
+++ DAY1_JavaScript_Drum_Kit/app.js
@@ -3,16 +3,16 @@ function removeTransition({propertyName, target}) {
     target.classList.remove('playing');
 }
 
-function playSound(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-  const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+function playSound({ keyCode }) {
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${keyCode}"]`);
 
   if (!audio) return
 
   audio.currentTime = 0;
   key.classList.add('playing');
   audio.play();
+}
   
-
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
