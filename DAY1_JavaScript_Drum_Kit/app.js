286f20cf299bfd677192570ee3a2354b962a8b3c
DominikMikuc <81574764+DominikMikuc@users.noreply.github.com>
2021-08-25 12:38:43 +0200

    Add files via upload

---
+++ DAY1_JavaScript_Drum_Kit/app.js
@@ -0,0 +1,24 @@
+function removeTransiton(e) {
+  if (e.propertyName !== 'transform') {
+    return
+  } else {
+    e.target.classList.remove('playing');
+  }
+}
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+
+  if (!audio) {
+    return
+  } else {    
+    audio.currentTime = 0;
+    key.classList.add('playing');
+    audio.play();
+  }
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransiton));
+window.addEventListener('keydown', playSound);
